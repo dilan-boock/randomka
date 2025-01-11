3250d25e475b97ac33d58a14766e40b11b0899c9
Dilan Zenn <u.corporeiscen@gmail.com>
2025-01-11 19:03:08 +0300

    Попытка сделать структ

--- main.go
+++ main.go
@@ -6,26 +6,32 @@ import (
 	"os"
 )
 
-var level, lev, chislo int
-var str string
+type Randomizer struct {
+	Level  int
+	Lev    int
+	Chislo int
+	I      int
+	Str    string
+}
+
+var num = Randomizer{0, 0, 0, 10, " "}
 
 func hello() {
 	fmt.Println("Выберите 1, 2 или 3")
-	fmt.Fscan(os.Stdin, &level)
-	randomizer()
+	fmt.Fscan(os.Stdin, &num.Level)
 }
 
 func play() {
 	for i := 10; i > 0; i-- {
-		fmt.Println(str)
-		fmt.Fscan(os.Stdin, &chislo)
-		if chislo < lev {
+		fmt.Println(num.Str)
+		fmt.Fscan(os.Stdin, num.Chislo)
+		if num.Chislo < num.Lev {
 			fmt.Println("Слишком мало, попробуйте еще раз. У вас осталось ", i-1, " попыток")
 		}
-		if chislo > lev {
+		if num.Chislo > num.Lev {
 			fmt.Println("Слишком много, попробуйте еще раз. У вас осталось ", i-1, " попыток")
 		}
-		if chislo == lev {
+		if num.Chislo == num.Lev {
 			fmt.Println("Вы выиграли!")
 			fmt.Println("Вы потратили ", 10-i, " попыток")
 			break
@@ -34,19 +40,19 @@ func play() {
 	fmt.Println("Вы проиграли, начните еще раз!")
 }
 func randomizer() {
-	switch lev = level; lev {
+	switch num.Lev = num.Level; num.Lev {
 	case 1:
-		lev = rand.Intn(100)
-		//fmt.Println("Ваш уровень ", level, "Ваш рандом = ", lev)
-		str = "Введите число от 0 до 100"
+		num.Lev = rand.Intn(100)
+		//fmt.Println("Ваш уровень ", num.Level, "Ваш рандом = ", num.Lev)
+		num.Str = "Введите число от 0 до 100"
 	case 2:
-		lev = rand.Intn(500)
-		//fmt.Println("Ваш уровень ", level, "Ваш рандом = ", lev)
-		str = "Введите число от 0 до 500"
+		num.Lev = rand.Intn(500)
+		//fmt.Println("Ваш уровень ", num.Level, "Ваш рандом = ", num.Lev)
+		num.Str = "Введите число от 0 до 500"
 	case 3:
-		lev = rand.Intn(1000)
+		num.Lev = rand.Intn(1000)
 		//fmt.Println("Ваш уровень ", level, "Ваш рандом = ", lev)
-		str = "Введите число от 0 до 1000"
+		num.Str = "Введите число от 0 до 1000"
 	default:
 		fmt.Println("Вы ошиблись цифрой")
 		hello()
@@ -55,5 +61,6 @@ func randomizer() {
 
 func main() {
 	hello()
+	randomizer()
 	play()
 }
